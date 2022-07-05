bf7685a5a5643229993ba4b51996733840ea43b2
mejinotdove <mejinotdove@users.noreply.github.com>
2022-07-05 21:43:20 +0800

    Create eh2qBittorrent.js

---
+++ eh2qBittorrent.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name        eh2qBittorrent
+// @namespace   takahashi
+// @include     *://exhentai.org/*
+// @require     https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @connect     *
+// @version     1.1
+// @author      takahashi
+// @description 2022/7/5 下午2:56:34
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  
+  unsafeWindow.sendDownload = function(btn) {    
+    var torrent_url = $(btn).siblings('a').attr('href');
+      
+    // 下载种子文件
+    GM_xmlhttpRequest({
+      method: 'GET',
+      url: torrent_url,
+      responseType:"blob",
+      onload: function(r) {
+        // 把种子编码成base64格式
+        var torrentBlob = r.response;
+        var formData = new FormData();
+        formData.append("torrents", torrentBlob, "test.torrent");
+                
+        // 发送添加种子请求
+        GM_xmlhttpRequest({
+          method: "POST",
+          responseType: "text",
+          url: GM_getValue('APIURL') + '/api/v2/torrents/add',
+          headers: {
+            "Origin": GM_getValue('APIURL')
+          },
+          data: formData,
+          onload: function(response){
+            if (response.status == 200) {
+              console.log('请求', response);
+              $('#torrentinfo > div:nth-child(1) > form > div > table > tbody > tr:nth-child(3) > td > a').parent().append('添加成功');
+            } else {
+              alert('添加失败!\nstatus code: ' + response.status + '\n详情: ' + response.response);
+            }
+          },
+          onerror: function(response){
+            console.log("请求失败", response);
+            alert('添加失败!', response);
+          }
+        });    
+    }});
+    
+  };
+  
+  $('#torrentinfo > div:nth-child(1) > form > div > table > tbody > tr:nth-child(3) > td > a').parent().append('<input type="button" value="下载" id="download" onclick="sendDownload(this)"/>');
+})();
